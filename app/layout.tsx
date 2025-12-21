d1316cad8d87ab666a1aa7a763b734638a13bde3
Kalvin RASAMY <kalvinrasamyzo@gmail.com>
2025-12-21 18:13:49 +0300

    fix: Add suppressHydrationWarning to body element.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+        suppressHydrationWarning
       >
         {children}
       </body>
